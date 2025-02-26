b17b1d1a9d71c9342047f51fa90cac4169db6e4b
Ji <overji1@sjtu.edu.cn>
2025-02-26 20:04:08 +0800

    完成了一部分个人页面

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,61 @@
 import { UserLayout } from "../generalUsages/Layout";
+import { Avatar, Row, Space, Typography } from "antd";
+import { getApiUrl } from "../services/common";
+import { useState, useEffect } from "react";
+import { changeIntroduction } from "../services/userAction";
+import "../stylesheets/User.css"
 
+const { Title, Paragraph } = Typography;
+
+function UserRow({children})
+{
+    return(
+        <Row className="UserRow">
+            {children}
+        </Row>
+    )
+}
 export default function UserPage() {
+    let userInfo = JSON.parse(sessionStorage.getItem("userInfo"));
+    const [introduction, setIntroduction] = useState('');
+
+    useEffect(() => {
+        if (userInfo) {
+            setIntroduction(userInfo.introduction);
+        }
+    }, [userInfo]);
+
+    if (!userInfo) {
+        return (
+            <UserLayout>
+                <p>出错了，请确认用户信息</p>
+            </UserLayout>
+        );
+    }
+
+    function updateIntroduction(value) {
+        changeIntroduction(value);
+    }
+
     return (
         <UserLayout>
-            <div>My Info: {sessionStorage.getItem("userInfo")}</div>
+            <Space direction="vertical" className="UserSpace">
+                <UserRow>
+                    <Avatar size={128} icon={<img src={`${getApiUrl()}/user/avatars/${userInfo.avatar}`} alt={`${userInfo.nickname}`} />} />
+                </UserRow>
+                <UserRow>
+                    <Title level={3}>{userInfo.nickname}</Title>
+                </UserRow>
+                <UserRow>
+                    <Paragraph
+                        editable={{
+                            onChange: updateIntroduction,
+                        }}
+                    >
+                        {introduction}
+                    </Paragraph>
+                </UserRow>
+            </Space>
         </UserLayout>
     );
 }
